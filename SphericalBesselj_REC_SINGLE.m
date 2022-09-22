c327b1274e903ef5a5b43ff6729d6f83d33a4b85
Funkmich008 <87016586+Funkmich008@users.noreply.github.com>
2022-09-22 15:17:36 +0200

    Add files via upload

---
+++ SphericalBesselj_REC_SINGLE.m
@@ -0,0 +1,41 @@
+% Reccurence Relation for the spherical Bessel functions of the first kind
+% for integer orders of n
+function j = SphericalBesselj_REC_SINGLE(n, x)   
+    if n>= 0
+        %% Reccurence for positive n
+        if n == 0
+            j = sin(x)./x;
+        elseif n == 1
+            j = sin(x)./x.^2 - cos(x)./x;
+        else
+            
+            j0 = sin(x)./x;
+            j1 = (sin(x)./x.^2 - cos(x)./x);
+    
+            j = 0;
+            for i=2:n
+                j = (2*(i-1)+1)./x .* j1 - j0;
+                j0 = j1;
+                j1 = j;
+            end
+        end
+    else
+        %% Reccurence for negative n
+        j0 = sin(x)./x;
+        j1 = (sin(x)./x.^2 - cos(x)./x);
+
+        j = 0;
+        for i=0:abs(n)-1
+            j = (-2*i+1)./x .* j0 - j1;          
+            j1 = j0;
+            j0 = j;
+        end
+    end
+
+    % limit at x = 0
+    if n == 0
+        j(x==0) = 1;
+    else
+        j(x==0) = 0;
+    end
+end
