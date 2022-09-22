494c6cd7c69bb8d39e5091b8cbc05096efc5f482
Funkmich008 <87016586+Funkmich008@users.noreply.github.com>
2022-09-22 15:26:59 +0200

    Add files via upload

---
+++ LAGUERRE_L.m
@@ -0,0 +1,17 @@
+% Berechnung der Laguerre-Polynome 1. Art per Rekurrenz-Formel
+function L = LAGUERRE_L(n, x)
+
+    if n == 0
+        L = 0 * x + 1;
+    elseif n == 1
+        L = 1 - x;
+    else
+       Ln2 = 1;
+       Ln1 = 1 - x;
+       for j = 2:n
+           L = (2*j-1-x)/j .* Ln1 - (j-1)/j * Ln2;
+           Ln2 = Ln1;
+           Ln1 = L;
+       end  
+    end
+end
