4d7d69cd0c4a9a9d5afb09b3e64eecbea322122f
Funkmich008 <87016586+Funkmich008@users.noreply.github.com>
2022-09-22 15:29:02 +0200

    Add files via upload

---
+++ LEGENDRE_P.m
@@ -0,0 +1,17 @@
+% Berechnung der gewöhnlichen Legendre-Polynome 1. Art per Rekurrenz-Formel
+function P = LEGENDRE_P(n, x)
+
+    if n == 0       
+        P = 0 * x + 1;       
+    elseif n == 1        
+        P = x;       
+    else        
+        Pn1 = 1;            % Startwert: n = 0
+        Pn = x;             % Startwert: n = 1
+        for j = 2:n
+            P = ((2*j-1) * x .* Pn - (j-1) * Pn1)./(j);
+            Pn1 = Pn;
+            Pn = P;            
+        end
+    end
+end
